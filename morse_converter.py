ae702fb37f5855e0c150a7298e70e54c0c2c1b7a
Virginia <virginia.mm41@gmail.com>
2024-05-10 16:49:53 +0200

    Initial Commit

---
+++ morse_converter.py
@@ -0,0 +1,31 @@
+# Python Scripting Project.
+# A text-based (command line) program that takes any String input and converts it into Morse Code.
+
+
+def text_to_morse(text):
+    morse_code_dict = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+        'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+        'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..',
+        '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+        '7': '--...', '8': '---..', '9': '----.',
+        '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.',
+        ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-',
+        '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+    }
+
+    morse_code = ""
+
+    for char in text:
+        if char in morse_code_dict:
+            morse_code += morse_code_dict[char] + " "
+        else:
+            morse_code += " "
+
+    return morse_code.strip()
+
+
+text = input("Type any text here: ").upper()
+morse_code = text_to_morse(text)
+print(f"Here is your converted message to morse code: {morse_code}")
